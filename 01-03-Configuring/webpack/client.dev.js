d7ec498deb905f8f5dd8679535d21bd81c56614c
Ian Jones <ian@egghead.io>
2018-12-07 10:13:50 -0500

    update to webpack 4 and babel 7

--- 01-03-Configuring/webpack/client.dev.js
+++ 01-03-Configuring/webpack/client.dev.js
@@ -1,49 +1,62 @@
-const path = require('path')
-const webpack = require('webpack')
-const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  mode: 'development',
   // REQUIRED: webpackHotServerMiddleware is expecting two webpack configs,
   // one with a name 'client', one with a name 'server'.
-  name: 'client',
+  name: "client",
   // Target browsers for our client config
-  target: 'web',
+  target: "web",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: "babel-loader"
       },
       {
         test: /\.css$/,
-        // REQUIRED: Extract css files into their own chunks
-        use: ExtractCssChunks.extract({
-          use: 'css-loader'
-        })
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../'
+            }
+          },
+          "css-loader"
+        ]
       }
     ]
   },
-  devtool: 'eval',
-  entry: path.resolve(__dirname, '../src/index.js'),
+  devtool: "eval",
+  entry: path.resolve(__dirname, "../src/index.js"),
   output: {
     // REQUIRED: file and chunk names should match
-    filename: '[name].js',
-    chunkFilename: '[name].js',
+    filename: "[name].js",
+    chunkFilename: "[name].js",
     // REQUIRED: where to write files to
-    path: path.resolve(__dirname, '../buildClient'),
+    path: path.resolve(__dirname, "../buildClient"),
     // REQUIRED: where files will be served from
-    publicPath: '/static/'
+    publicPath: "/static/"
   },
   plugins: [
     // REQUIRED: We have to initialize our ExtractCssChunks plugin
-    new ExtractCssChunks(),
-
-    // REQUIRED: Needed to put webpack bootstrap code before chunks
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['bootstrap'],
-      filename: '[name].js',
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      name: "bootstrap",
+      filename: "[name].js",
       minChunks: Infinity
-    }),
-  ]
-}
+    }
+  }
+};
